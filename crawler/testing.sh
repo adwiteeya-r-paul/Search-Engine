3cb3a73f72f051060d28c89cedb6aa41f481df97
Adwiteeya R Paul <153979848+adwiteeya-r-paul@users.noreply.github.com>
2025-06-30 23:55:10 +0600

    Updated testing.sh to rename testdirectories

--- crawler/testing.sh
+++ crawler/testing.sh
@@ -9,30 +9,30 @@
 # one argument                                                                      
 ./crawler http://cs50tse.cs.dartmouth.edu/tse/
 # two arguments                                                                     
-./crawler http://cs50tse.cs.dartmouth.edu/tse/ ../data
+./crawler http://cs50tse.cs.dartmouth.edu/tse/ ../testdirectory1
 # invalid URL                                                    
-./crawler http:// ../data 0
+./crawler http:// ../testdirectory1 0
 # invalid pageDirectory
 ./crawler http://cs50tse.cs.dartmouth.edu/tse/ ../datadata 0
 # maxDepth out of range
-./crawler http://cs50tse.cs.dartmouth.edu/tse/ ../data 100
+./crawler http://cs50tse.cs.dartmouth.edu/tse/ ../testdirectory1 100
 # maxDepth out of range
-./crawler http://cs50tse.cs.dartmouth.edu/tse/ ../data -1
+./crawler http://cs50tse.cs.dartmouth.edu/tse/ ../testdirectory1 -1
 # crawling letters till depth 0
-./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data 0
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../testdirectory1 0
 # crawling letters till depth 1
-./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data 1
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../testdirectory1 1
 # crawling letters till depth 10
-./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data 10
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../testdirectory1 10
 # crawling toscrape till depth 0
-./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/ ../data 0
+./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/ ../testdirectory1 0
 # crawling toscrape till depth 1
-./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/ ../data 1
+./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/ ../testdirectory1 1
 #crawling wikipedia till depth 0
-./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/ ../data 0
+./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/ ../testdirectory1 0
 #crawling wikipedia till depth 1
-./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/ ../data 1
+./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/ ../testdirectory1 1
 # Valgrind with letters 0
-valgrind ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data 0
+valgrind ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../testdirectory1 0
 # Valgrind with letters 1
-valgrind ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data 1
+valgrind ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../testdirectory1 1
